1c083683a73e9ef64dc19935d2e987b083038ade
maxmayev <maxmayev@gmail.com>
2023-09-08 22:15:50 +0300

    try2 - success

--- otus_java_2022_06/L21-jpql/homework-template/src/main/java/ru/otus/crm/model/Client.java
+++ otus_java_2022_06/L21-jpql/homework-template/src/main/java/ru/otus/crm/model/Client.java
@@ -46,6 +46,7 @@ public class Client implements Cloneable {
         this.id = id;
         this.name = name;
         this.address = address;
+        phones.forEach(phone -> phone.setClient(this));
         this.phones = phones;
     }
 
