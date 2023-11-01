b1b35673fcdc065e913de90a9cc3bc9e1cb3af23
maxmayev <maxmayev@gmail.com>
2023-11-01 19:09:18 +0300

    Изменен кодировщик паролей

--- Autograph/src/main/java/com/maxmayev/autograph/config/Config.java
+++ Autograph/src/main/java/com/maxmayev/autograph/config/Config.java
@@ -2,14 +2,14 @@ package com.maxmayev.autograph.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 public class Config {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new StandardPasswordEncoder("maxmayev");
+        return new BCryptPasswordEncoder();
     }
 }
